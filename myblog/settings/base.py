e27bbbeb90fbafa44bf5e3899c1024fc3b181461
rajpranit <rajpranit1207@gmail.com>
2022-06-07 19:21:13 +0530

    prod.py

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -3,7 +3,7 @@ import django_heroku
 import dj_database_url
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
---
+++ myblog/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-uni%#e+87cz0cuf+z5*eb!@k$zfu-*m0f*t2ca^y_*wgn@f(p8'
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'themanhwas.herokuapp.com',
+]
+
