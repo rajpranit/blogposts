b5b3c5375ee9d15f8cb4d3b516002caa6409414f
rajpranit <rajpranit1207@gmail.com>
2022-06-07 20:01:21 +0530

    commit9

--- myblog/settings/prod.py
+++ myblog/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-uni%#e+87cz0cuf+z5*eb!@k$zfu-*m0f*t2ca^y_*wgn@f(p8
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.beta800.net']
+ALLOWED_HOSTS = ['themanhwas.herokuapp.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
