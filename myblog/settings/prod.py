42a56fcab5e31508851388553d71cffdda532027
rajpranit <rajpranit1207@gmail.com>
2022-06-07 19:47:52 +0530

    commit5

--- myblog/settings/prod.py
+++ myblog/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 SECRET_KEY = 'django-insecure-uni%#e+87cz0cuf+z5*eb!@k$zfu-*m0f*t2ca^y_*wgn@f(p8'
 
@@ -17,3 +18,12 @@ INSTALLED_APPS = [
     # 'django.contrib.staticfiles',
     'weebs.apps.WeebsConfig',
 ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
