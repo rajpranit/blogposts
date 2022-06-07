568d8673777a3a1a5a235d52fbca35a3ba69d4cd
rajpranit <rajpranit1207@gmail.com>
2022-06-07 20:47:04 +0530

    commit12

--- myblog/settings/prod.py
+++ myblog/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-uni%#e+87cz0cuf+z5*eb!@k$zfu-*m0f*t2ca^y_*wgn@f(p8
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['themanhwas.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
 ]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    '/static/'
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
