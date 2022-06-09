801f3602c58e7de06eb307501a72abb73c744b06
rajpranit <rajpranit1207@gmail.com>
2022-06-09 23:53:46 +0530

    static

--- myblog/settings/dev.py
+++ myblog/settings/dev.py
@@ -144,6 +144,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'myblog.storage_backends.MediaStorage'
